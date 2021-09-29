b1594c4baf0ddfd11dd674751b770f73a3677ca8
JQ <jcq5010@gmail.com>
2021-09-29 12:08:23 -0400

    finished time logic, finished save logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,136 +1,142 @@
-let future = 3
-let present = 2
-let past = 1
 const container = $('.container');
 
-let timeSlots = [
-{
-    time: 8,
-    timeString: '8am',
-    todo: '',
-    state: past
-},
-{
-    time: 9,
-    timeString: '9am',
-    todo: '',
-    state: past
-},
-{
-    time: 10,
-    timeString: '10am',
-    todo: '',
-    state: present
-},
-{
-    time: 11,
-    timeString: '11am',
-    todo: '',
-    state: future
-},
-{
-    time: 12,
-    timeString: '12pm',
-    todo: '',
-    state: future
-},
-{
-    time: 1,
-    timeString: '1pm',
-    todo: '',
-    state: future
-},
-{
-    time: 2,
-    timeString: '2pm',
-    todo: '',
-    state: future
-},
-{
-    time: 3,
-    timeString: '3pm',
-    todo: '',
-    state: future
-},
-{
-    time: 4,
-    timeString: '4pm',
-    todo: '',
-    state: future
-},
-{
-    time: 5,
-    timeString: '5pm',
-    todo: '',
-    state: future
-},
-]
+let timeSlots;
 
+if(JSON.parse(localStorage.getItem('timeSlots')) == null){
+    timeSlots = [
+        {
+            time: '8',
+            timeString: '8am',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '9',
+            timeString: '9am',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '10',
+            timeString: '10am',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '11',
+            timeString: '11am',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '12',
+            timeString: '12pm',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '13',
+            timeString: '1pm',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '14',
+            timeString: '2pm',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '15',
+            timeString: '3pm',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '16',
+            timeString: '4pm',
+            todo: '',
+            state: 'past'
+        },
+        {
+            time: '17',
+            timeString: '5pm',
+            todo: '',
+            state: 'past'
+        },
+        ]
+} else {
+    timeSlots = JSON.parse(localStorage.getItem('timeSlots'))
+}
 
+const checkColor = (time, textArea) => {
+if((parseInt(time)) < parseInt(moment().format('k'))) {
+    if(!textArea.hasClass('past')){
+        textArea.addClass('past');
+       if(textArea.hasClass('present')){
+           textArea.removeClass('present')
+       };
+       if(textArea.hasClass('future')){
+           textArea.removeClass('future')
+       }
+    }
+} else if(parseInt(time) === parseInt(moment().format('k'))) {
+    if(!textArea.hasClass('present')){
+        textArea.addClass('present');
+       if(textArea.hasClass('past')){
+           textArea.removeClass('past')
+       };
+       if(textArea.hasClass('future')){
+           textArea.removeClass('future')
+       }
+    }
+}
+else if(parseInt(time) > parseInt(moment().format('k'))) {
+    if(!textArea.hasClass('future')){
+        textArea.addClass('future');
+       if(textArea.hasClass('past')){
+           textArea.removeClass('past')
+       };
+       if(textArea.hasClass('present')){
+           textArea.removeClass('present')
+       }
+    }
+}
+}
 
 const renderCalendar = () => {
     container.empty()
-    timeSlots.forEach(slot => {
-    let row = $('<div>').addClass('row')
-    let hourDiv = $('<div>').addClass('hour').text(slot.timeString)
-    let textArea = $('<textarea>').val(slot.todo)
-    let button = $('<button>').addClass('saveBtn').text('Save')
-    button.on('click', (e) => {
-        e.preventDefault();
-        console.log(e.target)
-    })
-
-    if(slot.state === past) {
-        row.addClass('past')
-    }
-    else if(slot.state === present) {
-        row.addClass('present')
-    }
-    else {
-        row.addClass('future')
-    }
-
-    row.append(hourDiv).append(textArea).append(button)
-    container.append(row);
+    timeSlots.forEach((slot) => {
+        let row = $('<div>').addClass('row')
+        let hourDiv = $('<div>').addClass('hour').text(slot.timeString)
+        let textArea = $('<textarea>').val(slot.todo)
+        let button = $('<button>').addClass('saveBtn').text('Save')
+        button.on('click', (e) => {
+            e.preventDefault();
+            slot.todo = textArea.val();
+            localStorage.setItem('timeSlots', JSON.stringify(timeSlots))
+        })
+        checkColor(slot.time, textArea)
+        setInterval(() => {
+            checkColor(slot.time, textArea)
+        }, 1000)
+        row.append(hourDiv).append(textArea).append(button)
+        container.append(row);
     })
     console.log('rendered')
-}
 
+}
 
 const updateTime = () => {
     // add time and date to header
     const hour = moment().format('hh:mm:ss');
     const dayOfWeek = moment().format('dddd'); 
     const dayOfYear = moment().format('MMMM Do');
-    const timeSpan = `${dayOfWeek}, ${dayOfYear}, ${hour}`
-    $('#currentDay').text(timeSpan)
-    // renderCalendar()
-
-    // style row depending on hour
-}
+    const timeSpan = `${dayOfWeek}, ${dayOfYear} ${hour}`;
+    $('#currentDay').text(timeSpan);
+};
 
 
-setInterval(updateTime, 1000)
-
-updateTime()
-
-function minuteCount(minutesAfterHour) {
-
-    const now          = new Date();
-    const hours        = now.getHours();
-    const minutes      = now.getMinutes();
-    const seconds      = now.getSeconds();
-    const milliseconds = now.getMilliseconds();
-
-    waitUntilNextMinute = setTimeout(minuteCount, 60000 - seconds * 1000 - milliseconds);
-
-    if(minutes % 60 === minutesAfterHour) {
-        // doSomethingHourly();
-        renderCalendar()
-        console.log('new hour')
-    }
-
-}
-
-minuteCount(0);
-
+updateTime();
+renderCalendar();
+setInterval(updateTime, 1000);
